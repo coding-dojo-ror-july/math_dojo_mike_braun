1c5b704bcef558b349ef45deac8ca79178663872
mpbraun2 <mpbraun2@gmail.com>
2017-07-05 18:21:09 +0000

    First commit

---
+++ main.rb
@@ -0,0 +1,22 @@
+class MathDojo
+  attr_accessor :result
+
+  def initialize
+    @result = 0
+  end
+
+  def add(*param)
+    @result += param.flatten.reduce(0, :+)
+    self
+  end
+
+  def subtract(*param)
+    @result -= param.flatten.reduce(0, :+)
+    self
+  end
+end
+
+challenge1 = MathDojo.new.add(2).add(2, 5).subtract(3, 2).result
+challenge2 = MathDojo.new.add(1).add([3, 5, 7, 8], [2, 4, 1]).subtract([2, 3], [1, 2]).result
+puts challenge1
+puts challenge2
